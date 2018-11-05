68103606c44a0ab72b805dcc17bda15ac88c2423
pove96 <sleivys.p@gmail.com>
2018-11-05 16:06:26 +0200

    Tvarkingas kodas

--- index.php
+++ index.php
@@ -1,26 +1,13 @@
-
 <html>
-    
     <body>
         <title>
             Aš valdau HTML <?php print "ir PHP"; ?> !
         </title>
-        
-        <h1>
-            
-         Povilas - HTML <?php print "ir PHP"; ?> asas!
-            
-        </h1> 
-        
+        <h1>Povilas - HTML <?php print "ir PHP"; ?> asas!</h1> 
         <p>
             Kuriu dinaminį turinį.
-            Atidaryta! <?php print date('l jS \of F Y h:i:s A'); 
-            ?>
-            
+            Atidaryta! <?php print date('l jS \of F Y h:i:s A'); ?>
         </p>
-    
-        
-       
+
     </body>
-    
 </html>
