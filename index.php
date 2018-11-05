b19a491c07c3f5fce42cf900445e00d806ed7f10
pove96 <sleivys.p@gmail.com>
2018-11-05 16:00:44 +0200

    trecia uzduotis datos printinimas

--- index.php
+++ index.php
@@ -1,21 +1,25 @@
+
 <html>
     
     <body>
         <title>
-            Aš valdau HTML!
+            Aš valdau HTML <?php print "ir PHP"; ?> !
         </title>
         
         <h1>
             
-         Povilas - HTML asas!
+         Povilas - HTML <?php print "ir PHP"; ?> asas!
             
         </h1> 
         
         <p>
             
-            Moku kurti statinį turinį!
+            Kuriu dinaminį turinį.
+            Atidaryta! <?php print date('l jS \of F Y h:i:s A'); 
+            ?>
             
         </p>
+    
         
        
     </body>
