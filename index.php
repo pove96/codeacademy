795a0fcc3813b4961e7b7993fdda0583d4ef5be8
pove96 <sleivys.p@gmail.com>
2018-11-06 09:45:55 +0200

    Day 2, užduotis 1 (alt+shift+f)

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
     <body>
         <title>
-            <?php print 'Aš,'.date('l').'ir PHP';?>
+            <?php print 'Aš,' . date('l') . 'ir PHP'; ?>
         </title>
-        <h1><b>Povilas</b> - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('I').' metų'?> </h1> 
+        <h1><b>Povilas</b> - HTML <?php print 'ir PHP'; ?> asas jau nuo <?php print date('Y') . ' metų' ?> </h1> 
         <p>
-            Viskas prasidėjo <?php print date('m').'mėnesio,'.date('m d').'dieną'?>
+            Viskas prasidėjo <?php print date('m') . 'mėnesio,' . date('d') . 'dieną' ?>
         </p>
     </body>
 </html>
