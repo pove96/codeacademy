af86201c33a18bd821f3d3552441c2e883c91739
pove96 <sleivys.p@gmail.com>
2018-11-06 15:19:09 +0200

    Day 2, užduotis 8, kauliukai randomaizinti

--- index.php
+++ index.php
@@ -2,24 +2,32 @@
 <html>
     <head> 
         <style>
+            .kauliukas {
+                width: 400px;
+                height: 400px;
+                background-size: cover;
+            }
             .class-1 {
-                color: red;
-                font-size: 100%;
+                background-image: url("http://clipart-library.com/images/kcKoqXL6i.gif");
             }
-            .class-2 {
-                color: blue;
-                font-size: 100%;
-            }
-            .class-3 {
-                color: green;
-                font-size: 100%;
-            }
-        </style>
-    <body>
-        <p class="class-<?php print rand(1, 3) ?>"> Vienas </p>
-        <p class="class-<?php print rand(1, 3) ?>"> Du </p>
-        <p class="class-<?php print rand(1, 3) ?>"> Trys </p>
-        
-    </body>
-</head>
-</html>
+                .class-2 {
+                    background-image: url("http://clipart-library.com/images/kcM8na5cj.png");
+                }
+                .class-3 {
+                    background-image: url("http://clipart-library.com/images/LidRo7Ei4.jpg");
+                }
+                .class-4 {
+                    background-image: url("http://clipart-library.com/images/pi5goLBi9.png");
+                }
+                .class-5 {
+                    background-image: url("https://upload.wikimedia.org/wikipedia/commons/d/dc/Dice-5.svg");
+                }
+                .class-6 {
+                    background-image: url("https://upload.wikimedia.org/wikipedia/commons/1/14/Dice-6.svg");
+                }
+            </style>
+        </head>
+        <body>
+            <div class="kauliukas class-<?php print rand(1, 6) ?>"></div>
+        </body>
+    </html>
