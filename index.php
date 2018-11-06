16fc2e4b81145b5eb8aecf4e509d84dace3e8dae
pove96 <sleivys.p@gmail.com>
2018-11-06 11:01:26 +0200

    Day 2, užduotis 3.1 (HTML commentai)

--- index.php
+++ index.php
@@ -4,11 +4,22 @@ date_default_timezone_set('Europe/Vilnius');
 <html>
     <body>
         <title>
-            PHP lydės ir <?php print 'ryt ' . date('d-M-Y', strtotime('+1 day')) ?>
+            Komentarai!
         </title>
-        <h1><b>Povilas</b> - PHP su manim buvo ir <?php print date('G', strtotime('-1 hour')) . ' valandą' ?> </h1> 
-        <p>
-            <?php print 'sekantys metai ' . date('Y', strtotime('+1 year')) ?> ne už kalnų!
-        </p>
+        <!--Pirmasis div blokas su h1 ir h2-->
+            <h1>
+                Aš pradedu!
+            </h1>
+            <h2>
+                O aš papildau!
+            </h2>
+        </div>
+        <!--<div>
+            <p>
+                Aš esu ta pana, <br>
+                kuri kalba ir kalba, <br>
+                ir kalba, ir kalba...
+            </p>
+        </div>-->
     </body>
 </html>
