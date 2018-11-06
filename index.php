3f0884ee471f12051aeaeba4a604f23bb1aad4b5
pove96 <sleivys.p@gmail.com>
2018-11-06 14:09:44 +0200

    Day 2, užduotis 6, random kvadratas

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="lt">
-    <head>
-        <meta charset="UTF-8">
-        <title>inline bbz</title>
-    </head>
-    <body style="background-color:rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255) ?>);">
-        <h1 style="font-size:<?php print rand(5, 50) . 'px' ?>">Tekstas</h1>
-        <p style="color:rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255) ?>)">D.Trump's nebebus prezidentu <?php print rand(2021, 2031) . ' ' . date('m d'); ?></p>
+<html>
+    <head> 
+        <style>
+            #Kvadratas{
+                position: absolute;
+                top: <?php print rand(0, 980); ?>px;
+                left: <?php print rand(0, 1820); ?>px;                  
+                width: 100px;
+                height: 100px;
+                background-color: red;
+            }
+        </style>
+    <body>
+        <div id="Kvadratas">
+        </div>
     </body>
+</head>
 </html>
