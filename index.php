e70dbac3e09bf9a0f2c31dd76783019d3dd74c44
pove96 <sleivys.p@gmail.com>
2018-11-07 11:38:05 +0200

    Day 3, integeriai xujegeriai ir visa kita xujne (uzduotis 2)

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
-<!DOCTYPE html>
-
 <?php
-$x = "tekstas";
-print "String: $x" . "<br>";
-$a = 1;
-print "Integer: $a" . "<br>";
-$b = 0.1;
-print "Float: $b" . "<br>";
-$c = false;
-print "Boolean: $c" . "<br>";
-$m = null;
-print "Null: $m" . "<br>";
+$src = 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,q_auto,w_640/v1/hellofresh_s3/image/55b9a2ec4dab7184418b4567.png';
+$href = 'https://cdn.newsapi.com.au/image/v1/e10debce170fd18a27f0fd3f024ec81e';
 ?>
+<html>
+    <head>
+        <title>
+
+        </title>
+    </head>
+    <body>
+        <p>
+            <img src="<?php print $src ?>">
+            <img src="<?php print $src ?>">
+            <img src="<?php print $src ?>">
+            <img src="<?php print $src ?>">
+
+            <a href="<?php print $href ?>"> Noriu valgyt!</a>
+            <a href="<?php print $href ?>"> Noriu valgyt!</a>
+            <a href="<?php print $href ?>"> Noriu valgyt!</a>
+            <a href="<?php print $href ?>"> Noriu valgyt!</a>
+        </p>
+    </body>
+</html>
