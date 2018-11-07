0c541d9e6c6108cbca8c22cbea6c981fabd9c82d
pove96 <sleivys.p@gmail.com>
2018-11-07 10:11:32 +0200

    Day 3, Bomba ir jos sprogimas

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
+<?php
+date_default_timezone_set('Europe/Vilnius');
+?>
 <html>
     <head> 
         <style>
-            .krc- {
-                display: none;
+            .bomba {
+                width: 100px;
+                height: 100px;
+                transform: scale(0.<?php print date('s') ?>);
+                background-image: url(https://files.gamebanana.com/img/ico/sprays/4ea33068c0dcc.png);
+                background-size: contain;
+                background-repeat: no-repeat;
             }
+            .sprogsta00 {
+                width: 400px;
+                height: 400px;
+                content: url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Operation_Upshot-Knothole_-_Badger_001.jpg/1024px-Operation_Upshot-Knothole_-_Badger_001.jpg);
+                <?php print date('s') ?>
+                transform: scale(1);
+            }
+
+
         </style>
     </head>
     <body>
-        <h1 class="krc-<?php print strpos(rand(1, 6), '1'); ?>">Laimejai interneta</h1>
+        <div class="sprogsta<?php print date('s') ?> bomba">
+        </div>
+        <p><?php print date('s'); ?></p>
+
     </body>
 </html>
