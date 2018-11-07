87945f4a20fd4f1481fb9e2db1f5d85f5cf65281
pove96 <sleivys.p@gmail.com>
2018-11-07 11:05:03 +0200

    Day 3, integeriai xujegeriai ir visa kita xujne (uzduotis 1) ( variable)

--- index.php
+++ index.php
@@ -1,33 +1,14 @@
 <!DOCTYPE html>
+
 <?php
-date_default_timezone_set('Europe/Vilnius');
+$x = "tekstas";
+print "String: $x" . "<br>";
+$a = 1;
+print "Integer: $a" . "<br>";
+$b = 0.1;
+print "Float: $b" . "<br>";
+$c = false;
+print "Boolean: $c" . "<br>";
+$m = null;
+print "Null: $m" . "<br>";
 ?>
-<html>
-    <head> 
-        <style>
-            .bomba {
-                width: 100px;
-                height: 100px;
-                transform: scale(0.<?php print date('s') ?>);
-                background-image: url(https://files.gamebanana.com/img/ico/sprays/4ea33068c0dcc.png);
-                background-size: contain;
-                background-repeat: no-repeat;
-            }
-            .sprogsta00 {
-                width: 400px;
-                height: 400px;
-                content: url(https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Operation_Upshot-Knothole_-_Badger_001.jpg/1024px-Operation_Upshot-Knothole_-_Badger_001.jpg);
-                <?php print date('s') ?>
-                transform: scale(1);
-            }
-
-
-        </style>
-    </head>
-    <body>
-        <div class="sprogsta<?php print date('s') ?> bomba">
-        </div>
-        <p><?php print date('s'); ?></p>
-
-    </body>
-</html>
