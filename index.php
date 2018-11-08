aad0d875c3a626f6cc710be970ef3142b9d7448b
pove96 <sleivys.p@gmail.com>
2018-11-08 08:56:31 +0200

    Day 4, Pirma uzduotis (kiek litru kuro tam tikram km kiekiui bei kaina)

--- index.php
+++ index.php
@@ -1,33 +1,23 @@
 <?php
 date_default_timezone_set('Europe/Vilnius');
 
-if (date('s') % 2 == 0) {
-    $a = 'kvadratas';
-} else {
-    $a = 'apskritimas';
-}
+$distance = rand(500, 1000);
+$fuel_100km = 7.5;
+$fuel_price = 1.3;
+$litrai = $distance / 13.33;
+$kaina = $litrai * 1.3;
+$litraisukaina = "Nuvažiavus " . $distance . "km" . " mašina sunaudos " . round($litrai) . " l. kuro" . " Kaina: " . round($kaina) . " " . " €";
 ?>
 <html>
     <head>
         <style>
-            .kvadratas {
-                background-image: url(https://upload.wikimedia.org/wikipedia/commons/d/dd/Square_-_black_simple.svg);
-            }
-            .apskritimas {
-                background-image: url(https://upload.wikimedia.org/wikipedia/commons/a/a0/Circle_-_black_simple.svg);
-            }
-            .klase {
-                height:100px;
-                width:100px;
-                background-size: cover;
-            }
+
         </style>
         <title>
 
         </title>
     </head>
     <body>
-        <div class="klase <?php print $a; ?>"><?php print date('s') ?></div>
-
+        <p><?php print $litraisukaina; ?> </p>
     </body>
 </html>
