b76cd003b84cca02b25f9d65c8fa4f1996b74499
pove96 <sleivys.p@gmail.com>
2018-11-09 11:35:59 +0200

    Day 5, Uzduotis su alum

--- index.php
+++ index.php
@@ -1,28 +1,51 @@
 <?php
-$months = 12;
-$starting_money = 1000;
-$alga = 700;
-$likutis = $starting_money;
-$visosislaidos = 0;
-for ($i = 1; $i < $months; $i++) {
-
-    $islaidos = rand(200, 350);
-    $visosislaidos += $islaidos;
-    $likutis += $alga - $islaidos;
-    if ($likutis < 0) {
-        break;
-    }
-}
-$vidutinesislaidos = round($visosislaidos / $months);
-$skaiciavimai = "Per $months Prognozuotų mėnesių, vidutinės išlaidos: " . $vidutinesislaidos . " Likutis pabaigoje $likutis";
+date_default_timezone_set('Europe/Vilnius');
+$bokalas = 'bokalas';
+$tulikas = 'tulikas';
+$beer_price = 3.49;
+$time_per_beer = 60;
 ?>
 <html
     <head>
         <style>
-            //Here we write the motherfucking style blyat
+            .tulikas{
+                background-image: url(http://www.stickpng.com/assets/images/589396308370b70e212f3e6a.png);
+                height: 100px;
+                width: 100px;
+                background-size: cover;
+            }
+            .bokalas {
+                background-image: url(https://mbtskoudsalg.com/images/alcohol-png-transparent-3.png);
+                height: 400px;
+                width: 200px;
+                background-size: cover;
+                background-size: contain;
+                background-repeat: no-repeat;
+                margin: 0 auto;
+            }
+            .numeriopozicija {
+                position: absolute;
+                top: 0;
+                right: 0;
+                background: black;
+                color: white;
+            }
         </style>
     </head>
     <body>
-        <p><?php print $skaiciavimai; ?></p>
+        <?php for ($i = 1; $i <= 7; $i++): ?>
+            <div style="position: relative; background-color:rgba(0, 0, 0, 0.<?php print $i ?>)">
+                <span class="numeriopozicija" style="text-align:right;">
+                    <?php print "Bokalo numeris " . $i . ", Iš viso išleidau " . ($i * $beer_price) . "€" ?>
+                </span>
+                <span class="numeriopizicija" style="text-align: left;">
+                    <?php print date('G, i', strtotime("+$i hours")); ?>
+                </span>
+                <div class="bokalas" style="filter: blur(<?php print $i ?>px)"></div>
+            </div>
+            <?php if ($i % 2 == 0): ?>
+                <div class="tulikas"></div>
+            <?php endif; ?>
+        <?php endfor; ?>
     </body>
 </html>
