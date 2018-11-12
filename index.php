dde76b03961cdd23281c3be027e982a69df5deb7
pove96 <sleivys.p@gmail.com>
2018-11-12 15:13:54 +0200

    Day 6, Daiktai is moters tases achujenas pavyzdys

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 $daiktu_pavadinimai = ['Kremas', 'Lakas', 'Pinigine'];
+$tases_turis = 0;
 $daiktuskaicius = rand(0, 5);
 $tase = [];
 for ($i = 0; $i < $daiktuskaicius; $i++) {
@@ -7,12 +8,16 @@ for ($i = 0; $i < $daiktuskaicius; $i++) {
     $spalva = rand(0, 1);
     $rand_pavad_idx = rand(0, count($daiktu_pavadinimai) - 1);
     $rand_pavad = $daiktu_pavadinimai[$rand_pavad_idx];
-
+    $tases_turis += $size;
     $tase[] = [
         'Pavadinimas' => $rand_pavad,
         'Dydis' => $size,
         'Spalva' => $spalva
     ];
+}
+
+foreach ($tase as $idx => $daiktas) {
+    $tase[$idx]['Tikimybe'] = $daiktas['Dydis'] / $tases_turis * 100;
 }
 ?>
 <html>
@@ -28,7 +33,7 @@ for ($i = 0; $i < $daiktuskaicius; $i++) {
     </head>
     <body>
         <?php foreach ($tase as $key => $produktas): ?>
-            <p><?php print $produktas['Pavadinimas'] . " uzima: " . $produktas['Dydis'] . " cm3 <br> Daikto spalva: " . ($produktas['Spalva'] ? 'sviesi' : 'tamsi'); ?></p>
+            <p><?php print $produktas['Pavadinimas'] . " uzima: " . $produktas['Dydis'] . " cm3 <br> Daikto spalva: " . ($produktas['Spalva'] ? 'sviesi' : 'tamsi') . " Tikimybė rasti: " . round($produktas['Tikimybe'], 2); ?></p>
         <?php endforeach; ?>
     </body>
 </html>
