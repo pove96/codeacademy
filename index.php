06aa7f8a2e4f6977db9617b3ff7aa1f6860a485d
pove96 <sleivys.p@gmail.com>
2018-11-13 09:58:12 +0200

    Day 7, Susigalvota uzduotis.

--- index.php
+++ index.php
@@ -1,17 +1,37 @@
 <?php
+date_default_timezone_set('Europe/Vilnius');
 
-function add($x, $y) {
-    $suma = $x + $y;
-    print "$x + $y suma: " . $suma;
+function datos($dabartine_valanda) {
+    $min_skirtumas = 99;
+    $datu_array = [
+        date("G", strtotime("+29 hours")),
+        date("G", strtotime("-35 hours")),
+        date("G", strtotime("+3 days"))
+    ];
+
+    $isrinktoji_valanda_is_arr = null;
+    foreach ($datu_array as $idx => $valanda) {
+
+        $skirtumas = abs($valanda - $dabartine_valanda);
+        if ($skirtumas < $min_skirtumas) {
+            $isrinktoji_valanda_is_arr = $valanda;
+            $min_skirtumas = $skirtumas;
+        }
+    }
+
+    return $isrinktoji_valanda_is_arr;
 }
+
+$artimiausia_valanda_is_arr = datos(date("G"));
 ?>
 <html>
     <head>
         <style>
-
+            /*The <style> tag is used to define 
+            style information for an HTML document.*/
         </style>
     </head>
     <body>
-        <h1><?php add(3, 5); ?></h1>
+        <h1><?php print $artimiausia_valanda_is_arr; ?></h1>
     </body>
 </html>
