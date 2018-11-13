4865603d71909f62ece343695a67930b6505386f
pove96 <sleivys.p@gmail.com>
2018-11-13 08:55:29 +0200

    Day 7, Funkcijos pirma uzduotis.

--- index.php
+++ index.php
@@ -1,39 +1,17 @@
 <?php
-$daiktu_pavadinimai = ['Kremas', 'Lakas', 'Pinigine'];
-$tases_turis = 0;
-$daiktuskaicius = rand(0, 5);
-$tase = [];
-for ($i = 0; $i < $daiktuskaicius; $i++) {
-    $size = rand(10, 60);
-    $spalva = rand(0, 1);
-    $rand_pavad_idx = rand(0, count($daiktu_pavadinimai) - 1);
-    $rand_pavad = $daiktu_pavadinimai[$rand_pavad_idx];
-    $tases_turis += $size;
-    $tase[] = [
-        'Pavadinimas' => $rand_pavad,
-        'Dydis' => $size,
-        'Spalva' => $spalva
-    ];
-}
 
-foreach ($tase as $idx => $daiktas) {
-    $tase[$idx]['Tikimybe'] = $daiktas['Dydis'] / $tases_turis * 100;
+function add($x, $y) {
+    $suma = $x + $y;
+    print "$x + $y suma: " . $suma;
 }
 ?>
 <html>
     <head>
         <style>
-            div {
-                width: 100px;
-                height: 100px;
-                background-size: cover;
-            }
 
         </style>
     </head>
     <body>
-        <?php foreach ($tase as $key => $produktas): ?>
-            <p><?php print $produktas['Pavadinimas'] . " uzima: " . $produktas['Dydis'] . " cm3 <br> Daikto spalva: " . ($produktas['Spalva'] ? 'sviesi' : 'tamsi') . " Tikimybė rasti: " . round($produktas['Tikimybe'], 2); ?></p>
-        <?php endforeach; ?>
+        <h1><?php add(3, 5); ?></h1>
     </body>
 </html>
