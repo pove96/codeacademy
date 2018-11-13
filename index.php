d6c68b295604a2f524185fcb25b0ed083b0dcc33
pove96 <sleivys.p@gmail.com>
2018-11-13 11:16:07 +0200

    Day 7, Isrinkti data is masyvo, kuri yra artimiausia dabartinei datai. Tvarkingas kodas su komentarais.

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
 <?php
 date_default_timezone_set('Europe/Vilnius');
 
-function datos($dabartine_valanda) {
-    $min_skirtumas = 99;
-    $datu_array = [
+/**
+ * Funkcija, kuri is masyvo suranda valanda, kuri yra artimiausia dabartinei valandai.
+ * if () Tikriname ar skirtumas yra mazesnis uz 99, jei mazesnis spausdiname ta valanda is masyvo
+ * @param type integer $find_closest_hour Dabartine valanda
+ * @return integer $closest_hour_of_the_array Valanda is masyvo, kuri buvo artimiausia dabartinei valandai
+ */
+function find_closest_hour($hour_find) {
+    //Masyvas su 3 betkokiomis valandomis
+    $hour_array_source = [
         date("G", strtotime("+29 hours")),
         date("G", strtotime("-35 hours")),
         date("G", strtotime("+3 days"))
     ];
 
-    $isrinktoji_valanda_is_arr = null;
-    foreach ($datu_array as $idx => $valanda) {
+    $hour_min_diff_from_source = 99;
+    //Artimiausia valanda - is pradziu undefined, nera lygi niekam
+    $closest_hour = null;
+    //Foreach ciklas, kuris pereina per visas valandas esancias masyve; $hour_source - valanda is masyvo
+    foreach ($hour_array_source as $hour_source) {
+        $hour_diff_from_source = abs($hour_find - $hour_source);
 
-        $skirtumas = abs($valanda - $dabartine_valanda);
-        if ($skirtumas < $min_skirtumas) {
-            $isrinktoji_valanda_is_arr = $valanda;
-            $min_skirtumas = $skirtumas;
+        if ($hour_diff_from_source < $hour_min_diff_from_source) {
+            $closest_hour = $hour_source;
         }
     }
 
-    return $isrinktoji_valanda_is_arr;
+    return $closest_hour;
 }
 
-$artimiausia_valanda_is_arr = datos(date("G"));
+$closest_hour_of_the_array = find_closest_hour(date("G"));
 ?>
 <html>
     <head>
@@ -32,6 +40,6 @@ $artimiausia_valanda_is_arr = datos(date("G"));
         </style>
     </head>
     <body>
-        <h1><?php print $artimiausia_valanda_is_arr; ?></h1>
+        <h1><?php print $closest_hour_of_the_array; ?></h1>
     </body>
 </html>
