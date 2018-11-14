9f142f87b536aecab0b5f28111f554b1c2c7ac69
pove96 <sleivys.p@gmail.com>
2018-11-14 15:42:22 +0200

    Day 8, Formos, masyvas su atsakymais ir klausimas, i forma pateikiam klausima ir gaunam atsakyma randomaizinta

--- index.php
+++ index.php
@@ -1,44 +1,23 @@
 <?php
-$drinks = [
-    'Vodka' => [
-        'pavadinimas' => 'Vodka',
-        'turis_litrais' => 0.7,
-        'prom' => 40
-    ],
-    'Alus' => [
-        'pavadinimas' => 'Alus',
-        'turis_litrais' => 0.5,
-        'prom' => 4.5
-    ]
-];
+$klausimas = null;
+$atsakymu_masyvas = ['Taip', 'Ne', 'Galbut'];
+if (isset($_POST['klausimas'])) {
 
-/**
- * Funkcija, kuri skaičiuoja, kiek butelių galima išgerti, kol gryno
- * alkoholio kiekis neviršys tam tikro kiekio litrais.
- * 
- * @param array $drinks Gėrimų masyvas su pavadinimais, tūriu bei promilėmis.<br>
- * $arr[0]<br>
- *      ['pavadinimas'] = 'Vodke';<br>
- *      ['prom'] = 40;<br>
- * 
- * @param real $max_level Maksimalus alokoholio kiekis, kiek gali išgerti.
- * @return array $drinks Gražiname $drinks masyvą.
- */
-function drinks($drinks, $max_level) {
-    //Ciklas, kuris pereina per visas gėrimų rūšis
-    foreach ($drinks as $idx => $drink_info) {
-        //Paskaičiuojame alkoholio kiekį
-        $alcohol_quantity = $drink_info['turis_litrais'] * $drink_info['prom'] / 100;
-        //Surandame kiek butelių galima išgerti
-        $bonkiu_skaicius = $max_level / $alcohol_quantity;
-        //Prie esamo masyvo pridedame naują bonkių skaičiaus informaciją
-        $drink_info['Bonkiu_skaicius'] = floor($bonkiu_skaicius);
-        $drinks[$idx] = $drink_info;
+    $klausimas = $_POST['klausimas'];
+    //print "Jusu klausimas: " . $klausimas;
+    if (strlen($klausimas) > 0) {
+        $atsakymas = atsakymu_masyvas($atsakymu_masyvas);
     }
-    return $drinks;
 }
 
-var_dump(drinks($drinks, 0.4));
+function atsakymu_masyvas($atsakymu_masyvas) {
+    $indexas = array_rand($atsakymu_masyvas, 1);
+    return $atsakymu_masyvas[$indexas];
+}
+
+//Taste some cookies biiiiiiiiiiiitch
+setcookie('klausimas', serialize($klausimas), time() + 3600);
+$data = unserialize($_COOKIE['klausimas']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -47,6 +26,13 @@ var_dump(drinks($drinks, 0.4));
         </style>
     </head>
     <body>
-        <h1> </h1>
+        <form action="index.php" method="POST">
+            Vardas: <input name="klausimas" type="text"/>
+            <input type="submit"/>
+        </form>
+<?php if (isset($atsakymas)): ?>
+            <h1> Jūsų klausimas: <?php print $klausimas ?></h1>
+            <h2> Atsakymas: <?php print $atsakymas ?></h2>
+<?php endif; ?>
     </body>
 </html>
